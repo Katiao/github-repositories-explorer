662bd381eb947563a9c2d789c4d0378acda4b798
unknown <katiao_@hotmail.co.uk>
2021-07-03 19:45:04 +0200

    add repo information toggle functionality

---
+++ src/components/SingleRepo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleRepo = () => {
+  return (
+    <div>
+      <h1>Single Repo</h1>
+    </div>
+  );
+};
+
+export default SingleRepo;
--- src/components/SingleUser.js
+++ src/components/SingleUser.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { useGithubContext } from "../context/context";
+import SingleRepo from "./SingleRepo";
 
 const SingleUser = ({ login }) => {
+  const { repos } = useGithubContext();
+
+  //setting state locally here as we only want state to affect this component and not other SigleUser components
+  const [isRepoOpen, setisRepoOpen] = useState(false);
+
+  const showRepo = () => {
+    setisRepoOpen(!isRepoOpen);
+  };
   return (
     <Wrapper>
-      <div className="userCard">{login}</div>
-      <MdKeyboardArrowDown className="arrow" />
+      <div className="user-card">
+        <div>{login}</div>
+        <MdKeyboardArrowDown className="arrow" onClick={showRepo} />
+      </div>
+      {isRepoOpen &&
+        repos.map((item) => {
+          return <SingleRepo key={item.id} {...item} />;
+        })}
     </Wrapper>
   );
 };
@@ -14,21 +30,23 @@ const SingleUser = ({ login }) => {
 export default SingleUser;
 
 const Wrapper = styled.article`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  max-width: 20rem;
-  border: 1px solid;
-  border-color: var(--clr-dark-grey);
-  color: var(--clr-primary-black);
-  background-color: var(--clr-light-grey);
-  padding: 0.4rem 0.3rem;
-  margin: 1rem auto;
-  border-radius: 2px;
+  .user-card {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    max-width: 20rem;
+    border: 1px solid;
+    border-color: var(--clr-dark-grey);
+    color: var(--clr-primary-black);
+    background-color: var(--clr-light-grey);
+    padding: 0.4rem 0.3rem;
+    margin: 1rem auto;
+    border-radius: 2px;
 
-  .arrow {
-    font-size: 1.2rem;
-    stroke-width: 0.02rem;
-    cursor: pointer;
+    .arrow {
+      font-size: 1.2rem;
+      stroke-width: 0.02rem;
+      cursor: pointer;
+    }
   }
 `;
